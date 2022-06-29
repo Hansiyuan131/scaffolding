c3d4319f46de0444128f1c41262fbdcf06d72672
Hansiyuan131 <422295068@qq.com>
2022-06-30 00:06:40 +0800

    yuan,feat: 添加sentinel流控

--- sca-service/sca-service-lottery/src/main/java/com/yuanstack/sca/service/lottery/controller/LotteryController.java
+++ sca-service/sca-service-lottery/src/main/java/com/yuanstack/sca/service/lottery/controller/LotteryController.java
@@ -1,14 +1,17 @@
 package com.yuanstack.sca.service.lottery.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.google.common.collect.Maps;
 import com.yuanstack.sca.service.lottery.config.SwitchConfig;
 import com.yuanstack.sca.service.lottery.service.LotteryService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @description: 抽奖控制器
@@ -40,5 +43,22 @@ public class LotteryController {
             log.info("动态开关 关闭");
             return "关";
         }
+    }
+
+    @GetMapping("/getAward")
+    @SentinelResource(value = "getAward")
+    public String getAward(@RequestParam("id") Long id) {
+        return "获取奖品";
+    }
+
+    @GetMapping("/getBatch")
+    @SentinelResource(value = "getTemplateInBatch", blockHandler = "getTemplateInBatchBlock")
+    public Map<Long, String> getTemplateInBatch(@RequestParam("ids") Collection<Long> ids) {
+        return new HashMap<>();
+    }
+
+    public Map<Long, String> getTemplateInBatchBlock(Collection<Long> ids, BlockException exception) {
+        log.info("接口被限流");
+        return Maps.newHashMap();
     }
 }
